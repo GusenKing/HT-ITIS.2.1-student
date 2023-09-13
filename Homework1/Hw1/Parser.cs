a664888de38f29796efd4ee059a5682f86808837
Emin <emin.sarux2004@yandex.ru>
2023-09-13 20:17:40 +0300

    Homework 1 done

--- Homework1/Hw1/Parser.cs
+++ Homework1/Hw1/Parser.cs
@@ -7,13 +7,41 @@ public static class Parser
         out CalculatorOperation operation, 
         out double val2)
     {
-        throw new NotImplementedException();
+        if (!IsArgLengthSupported(args))
+            throw new ArgumentException("Wrong amount of arguments");
+        
+        if (!Double.TryParse(args[0], out val1))
+            throw new ArgumentException("Values must be double");
+        operation = ParseOperation(args[1]);
+        if (!Double.TryParse(args[2], out val2))
+            throw new ArgumentException("Values must be double");
+
+        switch (args[1])
+        {
+            case "+":
+                operation = CalculatorOperation.Plus;
+                break;
+            case "-":
+                operation = CalculatorOperation.Minus;
+                break;
+            case "*":
+                operation = CalculatorOperation.Multiply;
+                break;
+            case "/":
+                operation = CalculatorOperation.Divide;
+                break;
+        }
     }
 
     private static bool IsArgLengthSupported(string[] args) => args.Length == 3;
 
-    private static CalculatorOperation ParseOperation(string arg)
-    {
-        throw new NotImplementedException();
-    }
+    private static CalculatorOperation ParseOperation(string arg) =>
+        arg switch
+        {
+            "+" => CalculatorOperation.Plus,
+            "-" => CalculatorOperation.Minus,
+            "*" => CalculatorOperation.Multiply,
+            "/" => CalculatorOperation.Divide,
+            _ => throw new InvalidOperationException("Wrong operation"),
+        };
 }
