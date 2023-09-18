871849558cdc5335bc9a627b0a65396b72b2d00b
Emin <emin.sarux2004@yandex.ru>
2023-09-18 19:29:38 +0300

    Hw2Caller main

---
+++ Homework2/Hw2Caller/Program.cs
@@ -0,0 +1,11 @@
+﻿using Hw2;
+
+try
+{
+    Parser.ParseCalcArguments(args, out var val1, out var operation, out var val2);
+    Console.WriteLine(Calculator.Calculate(val1, operation, val2));
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
