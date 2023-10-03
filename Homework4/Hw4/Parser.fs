680a99035003291e1c30dc003630fa0d3756ebe0
Emin <emin.sarux2004@yandex.ru>
2023-10-03 17:02:27 +0300

    current progress from laptop

--- Homework4/Hw4/Parser.fs
+++ Homework4/Hw4/Parser.fs
@@ -11,10 +11,26 @@ type CalcOptions = {
 }
 
 let isArgLengthSupported (args : string[]) =
-    NotImplementedException() |> raise
-
-let parseOperation (arg : string) =
-    NotImplementedException() |> raise
+    args.Length = 3
     
+let parseOperation (arg : string) =
+    match arg with
+    |"+" -> CalculatorOperation.Plus
+    |"-" -> CalculatorOperation.Minus
+    |"*" -> CalculatorOperation.Multiply
+    |"/" -> CalculatorOperation.Divide
+    |_ -> raise (ArgumentException("Wrong operation"))
+        
 let parseCalcArguments(args : string[]) =
-    NotImplementedException() |> raise
+    let mutable n1 = 0
+    let mutable n2 = 0
+    if not (isArgLengthSupported args) then ArgumentException("Incorrect amount of arguments") |> raise
+    if not Double.TryParse args[0] n1 then ArgumentException("First argument must be a number") |> raise
+    let parsedOperation = parseOperation args[1]
+    if not (Double.TryParse(args[2] n2)) then ArgumentException("Third argument must be a number") |> raise
+    
+    let result = {
+        arg1 = n1;
+        arg2 = n2;
+        operation = parsedOperation}    
+    result 
