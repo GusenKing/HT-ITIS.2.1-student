ca65ec40bff0e5b54db567f1c31bfd6d870c8939
Emin <emin.sarux2004@yandex.ru>
2023-11-10 08:58:43 +0300

    moved homework to this branch

--- Homework8/Hw8/Controllers/CalculatorController.cs
+++ Homework8/Hw8/Controllers/CalculatorController.cs
@@ -12,8 +12,23 @@ public class CalculatorController : Controller
         string operation,
         string val2)
     {
-        throw new NotImplementedException();
-    }
+        if (!double.TryParse(val1, NumberStyles.Float, CultureInfo.InvariantCulture, out var parsedVal1))
+            return new ObjectResult(Messages.InvalidNumberMessage);
+        if (!Enum.TryParse(operation, out Operation parsedOperation))
+            return new ObjectResult(Messages.InvalidOperationMessage);
+        if (!double.TryParse(val2, NumberStyles.Float, CultureInfo.InvariantCulture, out var parsedVal2))
+            return new ObjectResult(Messages.InvalidNumberMessage);
+
+        return parsedOperation switch
+        {
+            Operation.Plus => new ObjectResult(calculator.Plus(parsedVal1, parsedVal2)),
+            Operation.Minus => new ObjectResult(calculator.Minus(parsedVal1, parsedVal2)),
+            Operation.Multiply => new ObjectResult(calculator.Multiply(parsedVal1, parsedVal2)),
+            Operation.Divide => parsedVal2 == 0.0
+                ? new ObjectResult(Messages.DivisionByZeroMessage)
+                : new ObjectResult(calculator.Divide(parsedVal1, parsedVal2)),
+            _ => new ObjectResult(Messages.InvalidOperationMessage)
+        };    }
     
     [ExcludeFromCodeCoverage]
     public IActionResult Index()
