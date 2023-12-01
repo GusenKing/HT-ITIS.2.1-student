fa72916df04a22f3906a4aa5e3d599966f76ae71
Emin <emin.sarux2004@yandex.ru>
2023-12-01 10:37:20 +0300

    added test case to fox code coverage

--- Tests.CSharp/Homework11/IntegrationCalculatorControllerTests.cs
+++ Tests.CSharp/Homework11/IntegrationCalculatorControllerTests.cs
@@ -48,6 +48,7 @@ public class IntegrationCalculatorControllerTests : IClassFixture<WebApplication
     [InlineData(")10 + 6) * 3", MathErrorMessager.IncorrectBracketsNumber)]
     [InlineData("10 / 0", MathErrorMessager.DivisionByZero)]
     [InlineData("10 / (1 - 1)", MathErrorMessager.DivisionByZero)]
+    [InlineData("((10 + 6) - 9(", MathErrorMessager.IncorrectBracketsNumber)]
     public async Task Calculate_CalculateExpression_Error(string expression, string result)
     {
         var response = await CalculateAsync(expression);
