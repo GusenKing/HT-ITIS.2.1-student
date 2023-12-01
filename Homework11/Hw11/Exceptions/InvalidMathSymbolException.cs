f08b4abef6aad00038723467deccca77828a1b4a
Emin <emin.sarux2004@yandex.ru>
2023-12-01 11:21:14 +0300

    excluding exceptions from coverage

--- Homework11/Hw11/Exceptions/InvalidMathSymbolException.cs
+++ Homework11/Hw11/Exceptions/InvalidMathSymbolException.cs
@@ -1,7 +1,10 @@
-﻿namespace Hw11.Exceptions;
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Hw11.Exceptions;
 
 public class InvalidSymbolException: Exception
 {
+	[ExcludeFromCodeCoverage]
 	public InvalidSymbolException(string message)
 		: base(message)
 	{
